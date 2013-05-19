d31103da43071bfba3f5067077f1e0346e7cdabf
ashleybt <ashley.beattie@gmail.com>
2013-05-19 08:31:11 -0400

    add algorithms for low heart rate, high heart rate and arrythmia warnings

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,3 +1,25 @@
 class Reading < ActiveRecord::Base
   attr_accessible :BPM, :IBI, :time
+
+  @current_time = DateTime.now
+  @counter = 0
+
+  def calculate_BPM_every_4s
+
+    @events = Reading.limit(40)
+    @events.each do |event|
+      if event.BPM != 0
+        @counter = 0
+      else
+        @counter += 1
+      end
+    
+    if @counter == 40
+      Event.create(:event_type => "Arrythmia alert", :trigger => DateTime.now, :end => DateTime.now + 1)
+    else 
+      return nil
+    end
+
+  end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -7,29 +7,40 @@ Angelhackapp::Application.initialize!
 require "serialport"
 
 #params for serial port
-port_str = "/dev/tty.FireFly-CB97-RNI-SPP"  #may be different for you
-baud_rate = 9600
+port_str = "/dev/rfcomm0"  #may be different for you
+baud_rate = 115200
 data_bits = 8
 stop_bits = 1
 parity = SerialPort::NONE 
 
-sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+# sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
-counter = 0
+# counter = 0
 
-while true do
-  while (i = sp.gets.chomp) do       # see note 2
-    puts i
-		counter+=1
-	
-   if i[0] == "B" and counter == 10
-   		Reading.create(:time => DateTime.now, :BPM => ((i[1..3]).to_i)/2, :IBI => 0)
-   		counter = 0
- 			else
-   		ReadingIbi.create(:time => DateTime.now, :ibi => ((i[1..3]).to_i)/2)
-   	end
+# while true do
+#   while (i = sp.gets.chomp) do
+#     puts i
+		
+#     counter+=1
+	 
+#     age = User.age
+    
+#     factor = 2
+
+#     if i[0] == "B" and counter == 10
+#       Reading.create(:time => DateTime.now, :BPM => ((i[1..3]).to_i)/factor, :IBI => 0)
+#       counter = 0
+      
+#       if (((i[1..3]).to_i)/factor) > (190 + age)
+#         Event.create(:event_type => "high BPM alert", :trigger => DateTime.now, :end => DateTime.now + 1) 
+#       elsif (((i[1..3]).to_i)/factor) > (80 - age)
+#         Event.create(:event_type => "high BPM alert", :trigger => DateTime.now, :end => DateTime.now + 1)
+#       end
+#     else
+#    		ReadingIbi.create(:time => DateTime.now, :ibi => ((i[1..3]).to_i)/factor)
+#     end
    
-   end
-end
+#   end
+# end
 
-sp.close                       #see note 1
+# sp.close                       #see note 1
